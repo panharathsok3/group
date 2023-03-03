07f2a0836485d68c2229ec866c7540c701f2c464
Jessica <j.queen_xx@yahoo.com>
2023-03-03 17:51:04 -0500

    Added macro interface and bulk assign class. added executes macro

--- hw4/src/model/BulkAssignFilter.java
+++ hw4/src/model/BulkAssignFilter.java
@@ -7,6 +7,30 @@ public class BulkAssignFilter {
 
   Layer layer;
 
+  int startRow;
+
+  int startCol;
+
+  int endRow;
+  int endCol;
+
+  public void executesFilter() {
+    // should iterate through row and columns and set values
+    //i to length of rows
+    //j to length of columns
+
+    for(int i = this.startRow; i < this.endRow; i++) {
+      for(int j = this.startCol; i < this.endCol; i++) {
+        //get all the pixels on that layer and apply a filter onto it.
+
+      }
+    }
+
+
+  }
+
+
+
 
 
 }
