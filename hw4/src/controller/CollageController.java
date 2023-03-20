4a12b9d3e19b5ba26f26f296b90bb1edcc77ebb8
panharathsok3 <panharath.sok123@gmail.com>
2023-03-20 19:20:15 -0400

    added more comments for the controller

--- hw4/src/controller/CollageController.java
+++ hw4/src/controller/CollageController.java
@@ -6,7 +6,9 @@ package controller;
 public interface CollageController {
 
   /**
-   * The controller for the CollageProjectModelImpl.
+   * The controller for the CollageProjectModelImpl. This is used to take user input and give it
+   * to the model to process information and sends the information to the view to display something
+   * to the user.
    * @throws IllegalStateException if and only if the controller is unable to successfully read
    *                              input or transmit output
    */
